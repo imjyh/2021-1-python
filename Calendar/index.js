2378b0bb6d677cc6555bef1356ae4e05fa3cda29
nnjyh <imjyh01@naver.com>
2023-08-31 18:22:42 +0900

    [ADD] calendar initial setting

---
+++ Calendar/index.js
@@ -0,0 +1,6 @@
+const date = new Date();
+const viewYear = date.getFullYear();
+const viewMonth = date.getMonth();
+document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth +1}월`;
+const prevLast = new Date(viewYear, viewMonth, 0);
+const thisLast = new Date(viewYear, viewMonth+1, 0);
